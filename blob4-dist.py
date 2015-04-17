9c64b57451a8796246553def436ebe5856f3b8a0
Mandeep <mandeep.singh.bhatia@gmail.com>
2015-04-17 15:37:38 +0800

    blob4

--- blob4-dist.py
+++ blob4-dist.py
@@ -18,6 +18,16 @@ hwmaxs=[]
 bgr_max=[0,0,0]
 bgr_min=[255,255,255]
 
+
+minhws2=[]
+maxhws2=[]
+hwmins2=[]
+hwmaxs2=[]
+
+bgr_max2=[0,0,0]
+bgr_min2=[255,255,255]
+
+
 blur_size=4
 #green blur 4 (max for monitor cam)
 #[ 39 161 173]
@@ -69,6 +79,20 @@ def get_r(frm):
         cv2.waitKey(10)
     cv2.destroyWindow("cap")
 
+def get_r2(frm):
+    global x1,x2,y1,y2,stat
+    stat=0
+    cv2.namedWindow("cap2")
+    cv2.imshow("cap2",frm)
+    cv2.setMouseCallback("cap2",OnMouse)
+    while(stat<2):
+        if (stat==1):
+            im2=frm.copy()
+            cv2.rectangle(im2,(x1,y1),(x2,y2),(0,0,255,0),2,8,0)
+            cv2.imshow("cap2",im2)
+        cv2.waitKey(10)
+    cv2.destroyWindow("cap2")
+
 def find_rgb_r(frm):
     global bgr_max,bgr_min,x1,y1,x2,y2
     for x in range(x1,x2,1):
@@ -77,6 +101,15 @@ def find_rgb_r(frm):
             for cn in range(0,3):
                 if bgr[cn]>bgr_max[cn]:bgr_max[cn]=bgr[cn].copy()
                 if bgr[cn]<bgr_min[cn]:bgr_min[cn]=bgr[cn].copy()
+
+def find_rgb_r2(frm):
+    global bgr_max2,bgr_min2,x1,y1,x2,y2
+    for x in range(x1,x2,1):
+        for y in range(y1,y2,1):
+            bgr=frm[y,x]
+            for cn in range(0,3):
+                if bgr[cn]>bgr_max2[cn]:bgr_max2[cn]=bgr[cn].copy()
+                if bgr[cn]<bgr_min2[cn]:bgr_min2[cn]=bgr[cn].copy()
 
 
 def find_hsv(hsv):
@@ -113,6 +146,43 @@ def find_hsv(hsv):
                 hwmaxs[1]=sp
                 hwmaxs[2]=vp
             #look for h and s h1,s1 h2,s1 h1,s2 h2,s2
+
+def find_hsv2(hsv):
+    global minhws2,maxhws2,hwmins2,hwmaxs2,x1,y1,x2,y2
+
+    #we have the x1,y1,x2,y2
+    #print hsv[y1,x1]
+    hsr=hsv[y1,x1]
+    minhws2=hsr.copy()
+    maxhws2=hsr.copy()
+    hwmins2=hsr.copy()
+    hwmaxs2=hsr.copy()
+
+    for x in range(x1,x2,1):
+        for y in range(y1,y2,1):
+            pix=hsv[y,x]
+            hp=pix[0]
+            sp=pix[1]
+            vp=pix[2]
+            if hp<minhws2[0]:
+                minhws2[0]=hp
+                minhws2[1]=sp
+                minhws2[2]=vp
+            if hp>maxhws2[0]:
+                maxhws2[0]=hp
+                maxhws2[1]=sp
+                maxhws2[2]=vp
+            if sp<hwmins2[1]:
+                hwmins2[0]=hp
+                hwmins2[1]=sp
+                hwmins2[2]=vp
+            if sp>hwmaxs2[1]:
+                hwmaxs2[0]=hp
+                hwmaxs2[1]=sp
+                hwmaxs2[2]=vp
+            #look for h and s h1,s1 h2,s1 h1,s2 h2,s2
+
+
 cv2.namedWindow("brt")
 while(1):
     _,frame = cap.read()
@@ -131,6 +201,26 @@ find_rgb_r(frame)
 hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 find_hsv(hsv)
 
+
+cv2.namedWindow("brt2")
+while(1):
+    _,frame = cap.read()
+    cv2.imshow("brt2",frame)
+    if cv2.waitKey(33)==1048603:break
+
+cv2.destroyWindow("brt2")
+
+# smooth it
+frame = cv2.blur(frame,(blur_size,blur_size))#3,3
+
+get_r2(frame)
+find_rgb_r2(frame)
+
+# convert to hsv and find range of colors
+hsv2 = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+find_hsv2(hsv2)
+
+
 print minhws
 print maxhws
 print hwmins
@@ -138,17 +228,29 @@ print hwmaxs
 print "bgr"
 print bgr_min
 print bgr_max
+for ix in range(0,3):
+    if bgr_max[ix]<bgr_max2[ix]:bgr_max[ix]=bgr_max2[ix].copy()
+    if bgr_min[ix]>bgr_min2[ix]:bgr_min[ix]=bgr_min2[ix].copy()
+
 minv=minhws[2].copy()
 if minv>maxhws[2]:minv=maxhws[2].copy()
 if minv>hwmins[2]:minv=hwmins[2].copy()
 if minv>hwmaxs[2]:minv=hwmaxs[2].copy()
+
+if minv>maxhws2[2]:minv=maxhws2[2].copy()
+if minv>hwmins2[2]:minv=hwmins2[2].copy()
+if minv>hwmaxs2[2]:minv=hwmaxs2[2].copy()
 
 maxv=minhws[2].copy()
 if maxv<maxhws[2]:maxv=maxhws[2].copy()
 if maxv<hwmins[2]:maxv=hwmins[2].copy()
 if maxv<hwmaxs[2]:maxv=hwmaxs[2].copy()
 
-if (1):#gives error in bright colors thresholding?dont know why?numpy data type error
+if maxv<maxhws2[2]:maxv=maxhws2[2].copy()
+if maxv<hwmins2[2]:maxv=hwmins2[2].copy()
+if maxv<hwmaxs2[2]:maxv=hwmaxs2[2].copy()
+
+if (0):#gives error in bright colors thresholding?dont know why?numpy data type error
     if minv>90:minv=minv-10
     if maxv<240:maxv=maxv+10
     print minv
@@ -160,6 +262,12 @@ if(0):
 
 color_min=[minhws[0],hwmins[1],minv]#80
 color_max=[maxhws[0],hwmaxs[1],maxv]#255
+
+if (0):
+    vl=0.85#0.8,0.9
+    for ii in range(0,3):
+        bgr_min[ii]=int(bgr_min[ii]*vl)
+        #bgr_max[ii]=int(bgr_max[ii]*1.2)
 
 #cv2.namedWindow("rthresh")
 while(1):
